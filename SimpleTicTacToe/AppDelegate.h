404e3ce3b265382c844c31c4ff1b2e8c569ec81a
winxungphong <minhht@minhhts-macbook-pro.local>
2016-07-27 10:05:46 +0700

    Initial Commit

---
+++ SimpleTicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTicTacToe
+//
+//  Created by MinhHT on 7/27/16.
+//  Copyright © 2016 ME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTicTacToe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleTicTacToe
+//
+//  Created by MinhHT on 7/27/16.
+//  Copyright © 2016 ME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
